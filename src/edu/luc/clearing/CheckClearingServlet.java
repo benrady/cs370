2c05605fd2f5492e0ab61dfe6f8abd438911fec3
Ben Rady <brady@ubuntu.(none)>
2011-04-19 20:26:42 -0700

    Created basic project

---
+++ src/edu/luc/clearing/CheckClearingServlet.java
@@ -0,0 +1,11 @@
+package edu.luc.clearing;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CheckClearingServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("application/json");
+		resp.getWriter().print("{\"status\": \"ready\"}");
+	}
+}
