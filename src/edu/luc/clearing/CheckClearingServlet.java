1cdc51700d11c2e470e90b5851cfaf0926af0635
Ben Rady <brady@ubuntu.(none)>
2011-06-27 19:28:10 -0700

    Changed response

--- src/edu/luc/clearing/CheckClearingServlet.java
+++ src/edu/luc/clearing/CheckClearingServlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class CheckClearingServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("application/json");
-		resp.getWriter().print("{\"status\": \"ready\"}");
+		resp.getWriter().print("{}");
 	}
 }
