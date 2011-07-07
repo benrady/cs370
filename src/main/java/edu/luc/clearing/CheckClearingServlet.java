d307331b71909cc5b87d4cd70d2ba8ef1cc7522e
Ben Rady <benrady@gmail.com>
2011-07-06 22:44:27 -0500

    Replacing JSON library with Googles GSON

--- src/main/java/edu/luc/clearing/CheckClearingServlet.java
+++ src/main/java/edu/luc/clearing/CheckClearingServlet.java
@@ -1,14 +1,17 @@
 package edu.luc.clearing;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 @SuppressWarnings("serial")
 public class CheckClearingServlet extends HttpServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("application/json");
-		resp.getWriter().print("{}");
-	}
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().print(new Gson().toJson(new HashMap<String, Integer>()));
+    }
 }
